2d35968f52fcef767291de2c7d5aae089be3268c
NiitoT <niito_toyoda@outlook.jp>
2018-05-30 22:18:27 +0900

    ScriptTest２回目のコミットです
    課題と発展課題が入っています

---
+++ Assets/Test.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        // 要素数5の配列を初期化する
+        int[] points = { 0,10,20,30,40 };
+
+        // 配列の要素をすべて表示する
+        for (int i = 5 - 1 ; i >= 0; i--)
+        {
+            Debug.Log(points[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Test発展課題.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boss
+{
+    private int hp = 100;          // 体力
+    private int power = 25; // 攻撃力
+    private int mp = 53; //魔法値
+
+    // 攻撃用の関数
+    public void Attack()
+    {
+        
+    }
+
+    // 防御用の関数
+    public void Defence(int damage)
+    {
+        
+        // 残りhpを減らす
+        this.hp -= damage;
+    }
+    //魔法攻撃用の関数
+    public void Magic(int cost)
+    {
+        if (mp >= cost)
+        {
+            mp -= cost;
+            Debug.Log("魔法攻撃をした。残りMPは" + mp + "。");
+        }
+        else
+        {
+            Debug.Log("MPが足りないため魔法が使えない。");
+        }
+    }
+
+}
+
+public class Test発展課題 : MonoBehaviour
+{
+
+    void Start()
+    {
+        // Bossクラスの変数を宣言してインスタンスを代入
+        Boss lastboss = new Boss();
+
+        // 攻撃用の関数を呼び出す
+        lastboss.Attack();
+        // 防御用の関数を呼び出す
+        lastboss.Defence(3);
+        //Magic関数を呼び出して、魔法を使ってください
+        for (int i = 0; i < 11; i++)
+        {
+           lastboss.Magic(5);
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
